8050b23e6cd9ee2d8a8981372630260de7f23cc4
Apoorv Khandelwal <apoorvkhandelwal86@gmail.com>
2024-07-30 22:33:38 +0530

    Initial Commit

---
+++ src/components/ui/cards/cardwithform.tsx
@@ -0,0 +1,61 @@
+// In your component file, e.g., components/CardWithForm.tsx
+
+import React from 'react';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+
+export default function CardWithForm() {
+    return (
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>Create project</CardTitle>
+                <CardDescription>Deploy your new project in one-click.</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="name">Name</Label>
+                            <Input id="name" placeholder="Name of your project" />
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="framework">Framework</Label>
+                            <Select>
+                                <SelectTrigger id="framework">
+                                    <SelectValue placeholder="Select" />
+                                </SelectTrigger>
+                                <SelectContent position="popper">
+                                    <SelectItem value="nextjs">Next.js</SelectItem>
+                                    <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                                    <SelectItem value="astro">Astro</SelectItem>
+                                    <SelectItem value="nuxtjs">Nuxt.js</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button variant="outline">Cancel</Button>
+                <Button>Deploy</Button>
+            </CardFooter>
+        </Card>
+    );
+}
